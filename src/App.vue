aac2df4476341c9569f0eb19bd09635112718ff2
rca <nem@nem.x0.to>
2024-07-19 03:48:41 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
 </template>
 
 <script setup lang="ts">
+
+import useStore from 'src/store';
+import { getMeDetails } from './api/apiService';
+
+const store = useStore();
+
+// 非同期で更新
+getMeDetails().then((res) => {
+  store.commit('account/setId', res.id);
+  store.commit('account/setUsername', res.name);
+});
+
 defineOptions({
   name: 'App'
 });
