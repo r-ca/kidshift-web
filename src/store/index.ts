bc3999f440e67fd70378beac13b5fbe8f5afb44d
rca <nem@nem.x0.to>
2024-07-19 01:30:18 +0900

    vuex-persistedstate

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 import accountStateModule from './account';
 import { AccountStateInterface } from './account/state';
+import createPersistedState from 'vuex-persistedstate';
 
 export interface RootStateInterface {
   // Define your own store structure, using submodules if needed
@@ -12,6 +13,7 @@ export default function (/* { ssrContext } */) {
     modules: {
       account: accountStateModule
     },
+    plugins: [createPersistedState({storage: window.sessionStorage})],
 
     // enable strict mode (adds overhead!)
     // for dev mode and --debug builds only
