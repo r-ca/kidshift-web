1ac34ae3887324c5a8d54b232ae6638cbe97e79b
ろむねこ <nem@nem.x0.to>
2024-07-18 16:49:11 +0900

    Revert "test"
    This reverts commit ac55722f22470f2cb973346d05b4f8d7bfd17a96.

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,24 +19,22 @@ import {
  * with the Store instance.
  */
 
-export interface AccountStateInterface {
-  account: {
-    username: string
-    token: string
-  }
-  isLogin: boolean
-  taskList: any[]
+export interface StateInterface {
+  // Define your own store structure, using submodules if needed
+  // example: ExampleStateInterface;
+  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
+  example: unknown
 }
 
 // provide typings for `this.$store`
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $store: VuexStore<AccountStateInterface>
+    $store: VuexStore<StateInterface>
   }
 }
 
 // provide typings for `useStore` helper
-export const storeKey: InjectionKey<VuexStore<AccountStateInterface>> = Symbol('vuex-key')
+export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol('vuex-key')
 
 // Provide typings for `this.$router` inside Vuex stores
 declare module "vuex" {
@@ -46,8 +44,12 @@ declare module "vuex" {
   }
 }
 
-export default store(function(/* { ssrContext } */) {
-  const Store = createStore<AccountStateInterface>({
+export default store(function (/* { ssrContext } */) {
+  const Store = createStore<StateInterface>({
+    modules: {
+      // example
+    },
+
     // enable strict mode (adds overhead!)
     // for dev mode and --debug builds only
     strict: !!process.env.DEBUGGING
