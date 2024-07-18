6ea374cbb2bc546022ba70f49199b563dadccd1e
rca <nem@nem.x0.to>
2024-07-19 04:47:44 +0900

    マージン調整

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center q-my-md">
-    <q-card class="q-pa-md" style="max-width: 720px; width: 100%;">
+    <q-card class="q-pa-sm q-ma-sm" style="max-width: 720px; width: 100%;">
       <q-card-section>
         <div class="text-h6">タスク一覧</div>
       </q-card-section>
