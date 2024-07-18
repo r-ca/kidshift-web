be752b0c14dca7fa17ce894457585afa31f337a9
rca <nem@nem.x0.to>
2024-07-19 03:42:57 +0900

    reset

--- src/store/account/actions.ts
+++ src/store/account/actions.ts
@@ -1,16 +1,14 @@
 import { ActionTree } from 'vuex';
 import { RootStateInterface } from '../index';
 import { AccountStateInterface } from './state';
-import { getChildDetails } from 'src/api/apiService';
+import { getMeDetails } from 'src/api/apiService';
 
 const actions: ActionTree<AccountStateInterface, RootStateInterface> = {
   logout(context) {
     context.commit('setToken', '');
     context.commit('setUsername', '');
     context.commit('setLoggedIn', false);
-  },
-  fetchAccount(context) {
-    }
+  }
 };
 
 export default actions;
