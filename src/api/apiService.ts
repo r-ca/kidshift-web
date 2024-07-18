d3c424a2592abb07daf2ffc2d1270c1725a0dcbf
rca <nem@nem.x0.to>
2024-07-19 01:09:56 +0900

    get details作成

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,6 +1,20 @@
 import { api } from 'boot/axios';
+import { ChildDetailsResponse } from 'src/models/child';
 
 export const sendPing = async (): Promise<string> => {
-  const response = await api.get('/ping');
+  const response = await api.get('/meta/ping');
   return response.data;
 };
+
+export const loginWithCode = async (code: string): Promise<string> => {
+  const response = await api.post('/auth/login', { code });
+  return response.data.accessToken;
+}
+
+export const getChildDetails = async (childId: string): Promise<ChildDetailsResponse> => {
+  const response = await api.get(`/child/${childId}`);
+  if (response.status !== 200) {
+    throw new Error('ユーザー情報の取得に失敗しました');
+  }
+  return response.data;
+}
