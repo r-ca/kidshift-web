ce500cf7dd861703f052be96f6be0108436fd962
ろむねこ <nem@nem.x0.to>
2024-07-18 15:05:19 +0900

    blankレイアウト作成

---
+++ src/layouts/FullScreenLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.q-layout {
+  background: #f5f5f5;
+}
+</style>
