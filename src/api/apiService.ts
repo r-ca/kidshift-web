983e5976f5b470e676613a7ba539979df071bb43
rca <nem@nem.x0.to>
2024-07-19 01:59:17 +0900

    getTaskList実装

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,5 +1,6 @@
 import { api } from 'boot/axios';
 import { ChildDetailsResponse } from 'src/models/child';
+import { TaskListResponse } from 'src/models/task';
 
 export const sendPing = async (): Promise<string> => {
   const response = await api.get('/meta/ping');
@@ -15,6 +16,14 @@ export const getChildDetails = async (childId: string): Promise<ChildDetailsResp
   const response = await api.get(`/child/${childId}`);
   if (response.status !== 200) {
     throw new Error('ユーザー情報の取得に失敗しました');
+  }
+  return response.data;
+}
+
+export const getTaskList = async (): Promise<TaskListResponse> => {
+  const response = await api.get(`/task`);
+  if (response.status !== 200) {
+    throw new Error('タスク情報の取得に失敗しました');
   }
   return response.data;
 }
