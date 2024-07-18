a2ce177757c2dcb487ea599f6df92aec33fc78d1
ろむねこ <nem@nem.x0.to>
2024-07-18 16:36:42 +0900

    vuex wip

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { store } from 'quasar/wrappers';
 import { InjectionKey } from 'vue';
-import { createStore, Store as VuexStore, useStore as vuexUseStore } from 'vuex';
-import user from './accountState';
-import { UserState } from './user/types';
+import { Router } from 'vue-router';
+import { createStore, Store as VuexStore } from 'vuex';
+import accountState from './accountState';
+import { UserState } from './accountState/types';
 
 export interface StateInterface {
   user: UserState;
@@ -28,11 +29,8 @@ declare module 'vuex' {
 export default store(function() {
   const Store = createStore<StateInterface>({
     modules: {
-      user,
+      accountState,
     },
-    plugins: [createPersistedState({
-      paths: ['user.token', 'user.username', 'user.taskList'],
-    })],
   });
 
   return Store;
