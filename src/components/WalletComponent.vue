7d073e92c17c8f7a364eacc1a2c6b8c92d4f11df
rca <nem@nem.x0.to>
2024-07-21 22:56:43 +0900

    remove unused

--- src/components/WalletComponent.vue
+++ src/components/WalletComponent.vue
@@ -8,10 +8,7 @@
 <script setup lang="ts">
 import TotalComponent from 'components/TotalComponent.vue';
 import HistoryComponent from 'components/HistoryComponent.vue';
-import { getHistory } from 'src/api/apiService';
-import { TaskBaseItem } from 'src/models/task';
 import { ref } from 'vue';
-import { HistoryBaseItem } from 'src/models/history';
 import { getHistories } from 'src/service/historyService';
 import { HistoryItem } from 'src/models/internal';
 
