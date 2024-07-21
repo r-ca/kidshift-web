8b1e5ad87fb51d2fb8665c3b4834aebeea9b70b0
rca <nem@nem.x0.to>
2024-07-22 05:52:21 +0900

    チェックボックス追加 WIP

--- src/components/HistoryComponent.vue
+++ src/components/HistoryComponent.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="text-h6">
-    過去のお手伝い
+  <div class="text-h6" style="display: flex; justify-content: space-between; align-items: center;">
+    <span>過去のお手伝い</span>
+    <q-checkbox v-model="allChecked"></q-checkbox>
   </div>
   <q-list padding>
     <q-item v-for="(history, index) in props.histories" :key="index" clickable>
       <QIcon name="check" color="positive" class="q-my-auto q-mr-sm" v-if="history.isPaid" />
-      <QIcon name="schedule" color="warning" class="q-my-auto q-mr-sm" v-else/>
+      <QIcon name="schedule" color="warning" class="q-my-auto q-mr-sm" v-else />
       <q-item-section>
         <q-item-label>{{ history.name }}</q-item-label>
         <!-- <q-item-label caption>{{ task.date }}</q-item-label> -->
@@ -19,6 +20,7 @@
 
 <script setup lang="ts">
 import { HistoryItem } from 'src/models/internal';
+import { ref } from 'vue';
 
 interface Props {
   histories: HistoryItem[];
@@ -26,6 +28,8 @@ interface Props {
 
 // Props の取得
 const props = defineProps<Props>();
+
+const allChecked = ref(false);
 
 </script>
 
