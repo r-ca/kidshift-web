79d87df20ff9324a4dce45afa982779550e8cd8d
rca <nem@nem.x0.to>
2024-07-21 14:47:48 +0900

    リメイク wip

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,36 +1,24 @@
 <template>
-  <q-page class="flex flex-center q-my-md">
-    <q-card class="q-pa-sm q-ma-sm" style="max-width: 720px; width: 100%;">
-      <q-card-section>
-        <div class="text-h6">タスク一覧</div>
-      </q-card-section>
-      <q-card-section>
-        <q-tabs
-          v-if="isMobile"
-          class="text-grey-8"
-          active-color="primary"
-          indicator-color="primary"
-          v-model="tab"
-        >
-          <q-tab name="tab1" label="Tab 1" />
-          <q-tab name="tab2" label="Tab 2" />
-          <q-tab name="tab3" label="Tab 3" />
-        </q-tabs>
-
-        <q-tab-panels v-else v-model="tab" animated>
-          <q-tab-panel name="tab1">
+  <QPage class="flex flex-center q-my-md">
+    <QLayout view="hHh Lpr lFf">
+      <QPageContainer>
+        <QTabs v-if="isMobile" v-model="tab" class="text-teal" align="left" dense>
+          <QTab slot="title" name="tab1" v-model="tab" label="タスク一覧" />
+          <QTab slot="title" name="tab2" v-model="tab" label="ウォレット" />
+        </QTabs>
+        <QTabPanels v-if="isMobile" v-model="tab" animated>
+          <QTabPanel name="tab1">
             <TaskList />
-          </q-tab-panel>
-          <q-tab-panel name="tab2">
-            <!-- 他のコンテンツ -->
-          </q-tab-panel>
-          <q-tab-panel name="tab3">
-            <!-- 他のコンテンツ -->
-          </q-tab-panel>
-        </q-tab-panels>
-      </q-card-section>
-    </q-card>
-  </q-page>
+          </QTabPanel>
+        </QTabPanels>
+        <div v-else class="row">
+          <div class="col-6">
+            <TaskList />
+          </div>
+        </div>
+      </QPageContainer>
+    </QLayout>
+  </QPage>
 </template>
 
 <script setup>
