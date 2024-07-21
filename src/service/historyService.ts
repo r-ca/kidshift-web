0176d1b1880c4b33ff5c2eeb730989f59f3c8443
rca <nem@nem.x0.to>
2024-07-21 22:54:52 +0900

    突き合わせ + 変換メソッドを実装q

---
+++ src/service/historyService.ts
@@ -0,0 +1,26 @@
+import { HistoryListResponse, HistoryResponse } from "src/models/history";
+import { HistoryItem } from "src/models/internal";
+import { getHistory } from "src/api/apiService";
+import useStore from "src/store";
+
+const store = useStore();
+
+async function getHistories(childId: string, containPaid?: boolean): Promise<HistoryItem[]> {
+  const HistoriesResponse = await getHistory(childId, containPaid);
+  const taskList = store.state.cache.tasks; // use task
+
+  const result: HistoryItem[] = HistoriesResponse.list.map((history) => {
+    const task = taskList.find((task) => task.id === history.taskId);
+    return {
+      id: history.id,
+      name: task?.name || "Error!, please reload",
+      reward: task?.reward || 0,
+      isPaid: history.isPaid,
+    };
+  });
+
+  return result;
+}
+
+
+export { getHistories };
