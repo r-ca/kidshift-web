69aa061bf5209b1369863033845e0a34cfe6c9bd
rca <nem@nem.x0.to>
2024-07-21 22:58:48 +0900

    fix

--- src/components/HistoryComponent.vue
+++ src/components/HistoryComponent.vue
@@ -5,13 +5,13 @@
         過去のお手伝い
       </div>
       <q-list bordered padding>
-        <q-item v-for="(task, index) in props.histories" :key="index" clickable>
+        <q-item v-for="(history, index) in props.histories" :key="index" clickable>
           <q-item-section>
-            <q-item-label>{{ task.name }}</q-item-label>
+            <q-item-label>{{ history.name }}</q-item-label>
             <!-- <q-item-label caption>{{ task.date }}</q-item-label> -->
           </q-item-section>
           <q-item-section side>
-            <q-item-label>{{ task.reward }} 円</q-item-label>
+            <q-item-label>{{ history.reward }} 円</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
