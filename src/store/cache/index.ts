5b568bc387242e4edd015f385dedf5221faf1096
rca <nem@nem.x0.to>
2024-07-21 22:37:07 +0900

    fix

--- src/store/cache/index.ts
+++ src/store/cache/index.ts
@@ -5,7 +5,7 @@ import actions from './actions';
 import getters from './getters';
 import mutations from './mutations';
 
-const exampleModule: Module<CacheStateInterface, RootStateInterface> = {
+const cahceStateModule: Module<CacheStateInterface, RootStateInterface> = {
   namespaced: true,
   actions,
   getters,
@@ -13,4 +13,4 @@ const exampleModule: Module<CacheStateInterface, RootStateInterface> = {
   state
 };
 
-export default exampleModule;
+export default cahceStateModule;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,21 @@
 import { createStore } from 'vuex'
 import accountStateModule from './account';
+import cacheStateModule from './cache';
 import { AccountStateInterface } from './account/state';
 import createPersistedState from 'vuex-persistedstate';
+import { CacheStateInterface } from './cache/state';
 
 export interface RootStateInterface {
   // Define your own store structure, using submodules if needed
   account: AccountStateInterface;
+  cache: CacheStateInterface;
 }
 
 export default function (/* { ssrContext } */) {
   const store = createStore({
     modules: {
-      account: accountStateModule
+      account: accountStateModule,
+      cache: cacheStateModule
     },
     plugins: [createPersistedState({storage: window.localStorage})],
 
