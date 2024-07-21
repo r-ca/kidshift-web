93c71e51356b0dcb80507c7a97a20e2417084d0b
rca <nem@nem.x0.to>
2024-07-21 22:56:33 +0900

    実装修正

--- src/components/WalletComponent.vue
+++ src/components/WalletComponent.vue
@@ -12,6 +12,8 @@ import { getHistory } from 'src/api/apiService';
 import { TaskBaseItem } from 'src/models/task';
 import { ref } from 'vue';
 import { HistoryBaseItem } from 'src/models/history';
+import { getHistories } from 'src/service/historyService';
+import { HistoryItem } from 'src/models/internal';
 
 // Props の型定義
 interface Props {
@@ -21,10 +23,10 @@ interface Props {
 // Props の取得
 const props = defineProps<Props>();
 
-const tasks = ref([] as HistoryBaseItem[]);
+const tasks = ref([] as HistoryItem[]);
 
-getHistory(props.childId).then((res) => {
-  tasks.value = res.list;
+getHistories(props.childId, true).then((res) => {
+  tasks.value = res;
 });
 
 </script>
