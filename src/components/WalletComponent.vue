8f22158e8fc4a1925259e3defa51836c5dc5c79b
rca <nem@nem.x0.to>
2024-07-21 14:54:53 +0900

    fix

--- src/components/WalletComponent.vue
+++ src/components/WalletComponent.vue
@@ -9,8 +9,8 @@
 import TotalComponent from 'components/TotalComponent.vue';
 import HistoryComponent from 'components/HistoryComponent.vue';
 
-paymentAmount = 500; // お手伝いの報酬
-pastTasks = [
+const paymentAmount = 500; // お手伝いの報酬
+const pastTasks = [
   { name: '皿洗い', date: '2024-07-15', amount: 200 },
   { name: '掃除', date: '2024-07-14', amount: 300 },
   { name: '洗濯', date: '2024-07-13', amount: 250 },
